18ae431b1d7f2f4715a427166c4fb531c71fc436
Fabio Manganiello <fabio@manganiello.tech>
2026-03-01 22:29:27 +0100

    Bump version: 0.6.2 → 0.6.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def readfile(file):
 
 setup(
     name="madblog",
-    version="0.6.2",
+    version="0.6.3",
     author="Fabio Manganiello",
     author_email="info@fabiomanganiello.com",
     description="A minimal platform for Markdown-based blogs",
