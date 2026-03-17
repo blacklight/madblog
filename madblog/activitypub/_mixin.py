1a642c8acb929ed89b8192f49e28868827de06a4
Fabio Manganiello <fabio@manganiello.tech>
2026-03-17 23:53:57 +0100

    Revert "fix(activitypub): Attempted fix for verified URL on Mastodon"
    It doesn't work - it only turned the field into plaintext on Mastodon
    rather than a link.
    
    This reverts commit af9a5983fe01ca40f8237a1db529745a7f2ded51.

--- madblog/activitypub/_mixin.py
+++ madblog/activitypub/_mixin.py
@@ -156,7 +156,11 @@ class ActivityPubMixin(ABC):  # pylint: disable=too-few-public-methods
                 {
                     "type": "PropertyValue",
                     "name": config.activitypub_profile_field_name,
-                    "value": config.link,
+                    "value": (
+                        f'<a href="{config.link}" rel="me">'
+                        + re.sub(r"^(https?://)(.*)/*$", r"\2", config.link)
+                        + "</a>"
+                    ),
                 }
             )
 
@@ -279,7 +283,9 @@ class ActivityPubMixin(ABC):  # pylint: disable=too-few-public-methods
                     try:
                         fcntl.flock(f, fcntl.LOCK_EX | fcntl.LOCK_NB)
                     except BlockingIOError:
-                        logger.debug("Another worker is running startup sync, skipping")
+                        logger.debug(
+                            "Another worker is running startup sync, skipping"
+                        )
                         return
 
                     try:
